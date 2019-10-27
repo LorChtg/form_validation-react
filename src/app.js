6fd07851ac6963acba084f317f9e0b1b619da320
Lor Chtg <lorraine.chastang@gmail.com>
2019-10-27 19:28:40 -0300

    removed html form and created var for input pattern

--- src/app.js
+++ src/app.js
@@ -6,21 +6,25 @@ const Title = (props) => {
 
 class RegisterForm extends React.Component {
   render() {
+    const userNamePattern = "[A-Za-z\u00C0-\u00FF]{1,30}"
+    const mailPattern = "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+[\.]*[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@(gmail\.com)$"
+    const passwordPattern = "^(?=(?:[\u0000-\u007F\u0080-\u00FF]*[A-Z]){1})(?=(?:[\u0000-\u007F\u0080-\u00FF]*[a-z])+)(?=([\u0000-\u007F\u0080-\u00FF]*\d)+)(?=([\u0000-\u007F\u0080-\u00FF]*[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007F\u0080-\u00FF])+)[\u0000-\u007F\u0080-\u00FF]{9,30}$"
+    
     return (
-      <form className='form'>
+      <form className="form" onSubmit={e => e.preventDefault()}>
         <label for="username"  class="field">
           <span class="label">Username</span>
-          <input type="text" id="username" name="user_name" required pattern="[A-Za-z\u00C0-\u00FF]{1,30}" />
+          <input type="text" id="username" name="user_name" required pattern={userNamePattern} />
           <span class="error" aria-live="polite"></span>
         </label>
         <label for="mail" class="field">
           <span class="label">Email address</span>
-          <input type="email" id="email" name="user_email" required pattern="^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+[\.]*[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@(gmail\.com)$" />
+          <input type="email" id="email" name="user_email" required pattern={mailPattern} />
           <span class="error" aria-live="polite"></span>
         </label>
         <label for="password" class="field">
           <span class="label">Password</span>
-          <input type="password" id="password" name="user_password" required pattern="^(?=(?:[\u0000-\u007F\u0080-\u00FF]*[A-Z]){1})(?=(?:[\u0000-\u007F\u0080-\u00FF]*[a-z])+)(?=([\u0000-\u007F\u0080-\u00FF]*\d)+)(?=([\u0000-\u007F\u0080-\u00FF]*[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007F\u0080-\u00FF])+)[\u0000-\u007F\u0080-\u00FF]{9,30}$" />
+          <input type="password" id="password" name="user_password" required pattern={passwordPattern} />
           <span class="error" aria-live="polite"></span>
         </label>
         <button>Submit</button>
