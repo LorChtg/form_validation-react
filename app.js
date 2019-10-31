22d9c7db20a26478bd8148f13522e7b284acec34
Lor Chtg <lorraine.chastang@gmail.com>
2019-10-31 12:19:44 -0300

    state and npm start

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -22,65 +24,68 @@ var RegisterForm = function (_React$Component) {
 
     var _this = _possibleConstructorReturn(this, (RegisterForm.__proto__ || Object.getPrototypeOf(RegisterForm)).call(this, props));
 
-    _this.submit = _this.submit.bind(_this);
+    _this.handleChange = function (e) {
+      _this.setState(_defineProperty({}, e.target.name, e.target.value));
+      console.log(_this.state);
+    };
+
+    _this.handleSubmit = function (e) {
+      e.preventDefault();
+      console.log(_this.state);
+    };
+
+    _this.state = {
+      user_name: '',
+      user_email: '',
+      user_password: ''
+    };
+    _this.handleChange = _this.handleChange.bind(_this);
+    _this.handleSubmit = _this.handleSubmit.bind(_this);
     return _this;
   }
 
   _createClass(RegisterForm, [{
-    key: 'submit',
-    value: function submit(e) {
-      var _refs = this.refs,
-          _username = _refs._username,
-          _email = _refs._email,
-          _password = _refs._password;
-
-      e.preventDefault();
-      _username.value = '';
-      _email.value = '';
-      _password.value = '';
-    }
-  }, {
     key: 'render',
     value: function render() {
       var userNamePattern = '[A-Za-z\xC0-\xFF]{1,30}';
       var mailPattern = "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+[\.]*[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@(gmail\.com)$";
-      var passwordPattern = '^(?=(?:[\0-\x7F\x80-\xFF]*[A-Z]){1})(?=(?:[\0-\x7F\x80-\xFF]*[a-z])+)(?=([\0-\x7F\x80-\xFF]*d)+)(?=([\0-\x7F\x80-\xFF]*[\0-/:-@[-`{-\x7F\x80-\xFF])+)[\0-\x7F\x80-\xFF]{9,30}$';
+      var passwordPattern = '[\0-\x7F\x80-\xFF]{9,30}$';
 
       return React.createElement(
         'form',
-        { className: 'form', onSubmit: this.submit },
+        { className: 'form', onSubmit: this.handleSubmit, noValidate: true },
         React.createElement(
           'label',
-          { 'for': 'username', 'class': 'field' },
+          { htmlFor: 'username', className: 'field' },
           React.createElement(
             'span',
-            { 'class': 'label' },
+            { className: 'label' },
             'Username'
           ),
-          React.createElement('input', { ref: '_username', type: 'text', id: 'username', name: 'user_name', required: true, pattern: userNamePattern }),
-          React.createElement('span', { 'class': 'error', 'aria-live': 'polite' })
+          React.createElement('input', { onChange: this.handleChange, type: 'text', id: 'username', name: 'user_name', required: true, pattern: userNamePattern }),
+          React.createElement('span', { className: 'error', 'aria-live': 'polite' })
         ),
         React.createElement(
           'label',
-          { 'for': 'mail', 'class': 'field' },
+          { htmlFor: 'mail', className: 'field' },
           React.createElement(
             'span',
-            { 'class': 'label' },
+            { className: 'label' },
             'Email address'
           ),
-          React.createElement('input', { ref: '_email', type: 'email', id: 'email', name: 'user_email', required: true, pattern: mailPattern }),
-          React.createElement('span', { 'class': 'error', 'aria-live': 'polite' })
+          React.createElement('input', { onChange: this.handleChange, type: 'email', id: 'email', name: 'user_email', required: true, pattern: mailPattern }),
+          React.createElement('span', { className: 'error', 'aria-live': 'polite' })
         ),
         React.createElement(
           'label',
-          { 'for': 'password', 'class': 'field' },
+          { htmlFor: 'password', className: 'field' },
           React.createElement(
             'span',
-            { 'class': 'label' },
+            { className: 'label' },
             'Password'
           ),
-          React.createElement('input', { ref: '_password', type: 'password', id: 'password', name: 'user_password', required: true, pattern: passwordPattern }),
-          React.createElement('span', { 'class': 'error', 'aria-live': 'polite' })
+          React.createElement('input', { onChange: this.handleChange, type: 'text', id: 'password', name: 'user_password', required: true, pattern: passwordPattern }),
+          React.createElement('span', { className: 'error', 'aria-live': 'polite' })
         ),
         React.createElement(
           'button',
--- src/app.js
+++ src/app.js
@@ -7,38 +7,48 @@ const Title = (props) => {
 class RegisterForm extends React.Component {
   constructor(props) {
     super(props)
-    this.submit = this.submit.bind(this)
+    this.state = {
+      user_name: '',
+      user_email: '',
+      user_password: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  submit(e) {
-    const {_username, _email, _password} = this.refs
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    },)
+    console.log(this.state)
+  }
+
+  handleSubmit = (e) => {
     e.preventDefault()
-    _username.value = ''
-    _email.value = ''
-    _password.value = ''
+    console.log(this.state)
   }
 
   render() {
     const userNamePattern = "[A-Za-z\u00C0-\u00FF]{1,30}"
     const mailPattern = "^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+[\.]*[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+@(gmail\.com)$"
-    const passwordPattern = "^(?=(?:[\u0000-\u007F\u0080-\u00FF]*[A-Z]){1})(?=(?:[\u0000-\u007F\u0080-\u00FF]*[a-z])+)(?=([\u0000-\u007F\u0080-\u00FF]*\d)+)(?=([\u0000-\u007F\u0080-\u00FF]*[\u0000-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007F\u0080-\u00FF])+)[\u0000-\u007F\u0080-\u00FF]{9,30}$"
+    const passwordPattern = "[\u0000-\u007F\u0080-\u00FF]{9,30}$"
     
     return (
-      <form className="form" onSubmit={this.submit}>
-        <label for="username"  class="field">
-          <span class="label">Username</span>
-          <input ref="_username" type="text" id="username" name="user_name" required pattern={userNamePattern} />
-          <span class="error" aria-live="polite"></span>
+      <form className="form" onSubmit={this.handleSubmit} noValidate>
+        <label htmlFor="username"  className="field">
+          <span className="label">Username</span>
+          <input onChange={this.handleChange} type="text" id="username" name="user_name" required pattern={userNamePattern} />
+          <span className="error" aria-live="polite"></span>
         </label>
-        <label for="mail" class="field">
-          <span class="label">Email address</span>
-          <input ref="_email" type="email" id="email" name="user_email" required pattern={mailPattern} />
-          <span class="error" aria-live="polite"></span>
+        <label htmlFor="mail" className="field">
+          <span className="label">Email address</span>
+          <input onChange={this.handleChange} type="email" id="email" name="user_email" required pattern={mailPattern} />
+          <span className="error" aria-live="polite"></span>
         </label>
-        <label for="password" class="field">
-          <span class="label">Password</span>
-          <input ref="_password" type="password" id="password" name="user_password" required pattern={passwordPattern} />
-          <span class="error" aria-live="polite"></span>
+        <label htmlFor="password" className="field">
+          <span className="label">Password</span>
+          <input onChange={this.handleChange} type="text" id="password" name="user_password" required pattern={passwordPattern} />
+          <span className="error" aria-live="polite"></span>
         </label>
         <button>Submit</button>
 
